222e67c0ab8c4182d148b22fbdfd3cf76dae4116
Jorge-Alda <jalda@unizar.es>
2022-11-09 17:12:26 +0100

    Fixed tests for LikelihoodResults

--- likelihoodfits/test_classes.py
+++ likelihoodfits/test_classes.py
@@ -39,63 +39,72 @@ class TestLikelihoodValues:
 class TestLikelihoodResults:
     x = likelihoodfits.Axis([0, 1, 2, 3, 4], 'x', 'x')
     y = likelihoodfits.Axis([6, 7, 8, 9, 10], 'y', 'y')
-    lhr = likelihoodfits.LikelihoodResults(x, y)
 
     def test_numdata(self):
-        assert self.lhr.numdata == 25
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
+        assert lhr.numdata == 25
 
     def test_add_likelihood(self):
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
         lh_rand = likelihoodfits.LikelihoodValues(
             np.random.random((5, 5)), 'random', 'rnd')
-        self.lhr.add_likelihood(lh_rand)
-        assert self.lhr.likelihoods[-1].likelihood == 'random'
+        lhr.add_likelihood(lh_rand)
+        assert lhr.likelihoods[-1].likelihood == 'random'
 
     def test_wrong_add_likelihood(self):
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
         lh_rand = likelihoodfits.LikelihoodValues(
             np.random.random((6, 5)), 'random', 'rnd')
         with pytest.raises(ValueError):
-            self.lhr.add_likelihood(lh_rand)
+            lhr.add_likelihood(lh_rand)
 
     def test_new_likelihood(self):
-        self.lhr.new_likelihood('testnew', 'testnew')
-        assert self.lhr.likelihoods[-1].likelihood == 'testnew'
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
+        lhr.new_likelihood('testnew', 'testnew')
+        assert lhr.likelihoods[-1].likelihood == 'testnew'
 
     def test_calculate_point(self):
-        def fun(x, y): return {'testnew': x*y, 'random': 4}
-        self.lhr.new_likelihood('testnew', 'testnew')
-        self.lhr.calculate_point(fun, 3, 1)
-        assert self.lhr.likelihoods[-1].data[1, 3] == 21  # Está transpuesto
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
+        def fun(x, y): return {'testnew': x*y}
+        lhr.new_likelihood('testnew', 'testnew')
+        lhr.calculate_point(fun, 3, 1)
+        assert lhr.likelihoods[-1].data[1, 3] == 21  # Está transpuesto
 
     def test_calculate_all(self):
-        def fun(x, y): return {'testnew': x*y, 'random': 4}
-        self.lhr.new_likelihood('testnew', 'testnew')
-        self.lhr.calculate_all(fun)
-        assert self.lhr.likelihoods[-1].data[2, 3] == 24
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
+        def fun(x, y): return {'testnew': x*y}
+        lhr.new_likelihood('testnew', 'testnew')
+        lhr.calculate_all(fun)
+        assert lhr.likelihoods[-1].data[2, 3] == 24
 
     def test_calculate_missing_lh(self):
-        def fun(x, y): return {'testold': x*y, 'random': 4}
-        self.lhr.new_likelihood('testnew', 'testnew')
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
+        def fun(x, y): return {'testold': x*y}
+        lhr.new_likelihood('testnew', 'testnew')
         with pytest.raises(KeyError):
-            self.lhr.calculate_point(fun, 3, 1)
+            lhr.calculate_point(fun, 3, 1)
 
     def test_save(self):
-        def fun(x, y): return {'testnew': x*y, 'random': 4}
-        self.lhr.new_likelihood('testnew', 'testnew')
-        self.lhr.calculate_all(fun)
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
+        def fun(x, y): return {'testnew': x*y}
+        lhr.new_likelihood('testnew', 'testnew')
+        lhr.calculate_all(fun)
         try:
             with BytesIO() as file:
-                self.lhr.to_hdf5(file)
-        except:
+                lhr.to_hdf5(file)
+        except IOError as e:
+            print(e)
             saved = False
         else:
             saved = True
         assert saved
 
     def test_load(self):
-        def fun(x, y): return {'testnew': x*y, 'random': 4}
-        self.lhr.new_likelihood('testnew', 'testnew')
-        self.lhr.calculate_all(fun)
+        lhr = likelihoodfits.LikelihoodResults(self.x, self.y)
+        def fun(x, y): return {'testnew': x*y}
+        lhr.new_likelihood('testnew', 'testnew')
+        lhr.calculate_all(fun)
         with BytesIO() as file:
-            self.lhr.to_hdf5(file)
+            lhr.to_hdf5(file)
             lh2 = likelihoodfits.LikelihoodResults.from_hdf5(file)
         assert lh2.likelihoods[0].data[2, 3] == 24
